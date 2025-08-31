3cb978ce0cb1b19fd3243787f2c6ea0fe5037f18
Svcre  .B <165298198+svcre0@users.noreply.github.com>
2025-08-31 04:16:22 -0700

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import heroImage from "@/assets/backgrond.jpeg";
+import heroImage from "@/assets/backgrond.jpg";
 
 const HeroSection = () => {
   return (
